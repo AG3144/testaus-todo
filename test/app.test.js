af57f061fbfe229104405e45a9e2e19219e82513
Teemu Sairi <ag3144@student.jamk.fi>
2025-11-20 14:08:02 +0200

    testipohja

---
+++ test/app.test.js
@@ -0,0 +1,6 @@
+import { describe, test, expect, it } from 'vitest';
+const Laskin = require('../public/app.js');
+
+describe('Laskimen testaus', function () {
+  it('should do stuff', function () {});
+});
